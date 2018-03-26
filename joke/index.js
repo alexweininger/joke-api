1597ee973300eb07162e5b2b66ae9bd9dbbf7b3d
alexweininger <alex.weininger@live.com>
2018-03-25 21:38:07 -0700

    get random joke

--- joke/index.js
+++ joke/index.js
@@ -1,19 +1,27 @@
 var jokes = require('../jokes/index.json');
-
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.query.joketype || (req.body && req.body.joketype)) {
+        var jokesOfType = [];
+        for (joke in jokes) {
+            if (joke.type == req.query.joketype) {
+                jokesOfType.push(joke);
+                context.log('joke');
+            }
+        }
+        let random_index = Math.floor(Math.random() * jokesOfType.length);
+        var rjoke = jokesOfType[random_index];
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: JSON.stringify(jokes[0])
+            body: JSON.stringify(rjoke)
+        }
+    } else {
+        let random_index = Math.floor(Math.random() * jokes.length);
+        var r_joke = jokes[random_index];
+        context.res = {
+            body: JSON.stringify(r_joke)
         };
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+
     context.done();
 };
