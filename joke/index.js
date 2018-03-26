92ba433f97739d9764832f6ec810b501dfc332bd
alexweininger <alex.weininger@live.com>
2018-03-25 20:54:16 -0700

    first commit

---
+++ joke/index.js
@@ -0,0 +1,19 @@
+var jokes = require('../jokes/index.json');
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: JSON.stringify(jokes[0])
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
