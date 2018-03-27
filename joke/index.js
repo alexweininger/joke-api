a9e220a9f4e4ebd22eafb13c41bfecf906a50ce4
alexweininger <alex.weininger@live.com>
2018-03-27 16:24:38 -0700

    cleanup up and restructured api logic

--- joke/index.js
+++ joke/index.js
@@ -1,25 +1,40 @@
 var jokes = require('../jokes/index.json');
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    context.log(req.query);
     if (req.query.joketype || (req.body && req.body.joketype)) {
-        var jokesOfType = [];
-        for (joke of jokes) {
-            if (joke.type == req.query.joketype) {
-                jokesOfType.push(joke);
-            }
-        }
-        let random_index = Math.floor(Math.random() * jokesOfType.length);
-        var rjoke = jokesOfType[random_index];
+        let joke = getRandomElement(getType(req.query.joketype, jokes));
         context.res = {
-            body: rjoke
+            body: JSON.stringify(joke)
         }
     } else {
-        let random_index = Math.floor(Math.random() * jokes.length);
-        var r_joke = jokes[random_index];
+        let joke = getRandomElement(jokes)
         context.res = {
-            body: r_joke
+            body: JSON.stringify(joke)
         };
     }
     context.done();
 };
+
+function getRandomElement(array) {
+    let randIndex = Math.floor(Math.random() * array.length);
+    return array[randIndex];
+}
+
+function getType(type, jokes) {
+    let jokeArray = [];
+    for (let joke of jokes) {
+        if (joke.type == type) {
+            jokeArray.push(joke);
+        }
+    }
+    return jokeArray;
+}
+
+function getJokeById(id, jokes){
+    for(let joke of jokes){
+        if(joke.id === id){
+            return joke;
+        }
+    }
+}
